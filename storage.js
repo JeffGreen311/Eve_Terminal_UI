c71c529eeee580f3000d1c3bd46644a892eabc21
Jeff Green <jeffgreen311@gmail.com>
2026-01-07 10:17:36 -0600

    Delete storage.js
    Signed-off-by: Jeff Green <jeffgreen311@gmail.com>

--- storage.js
+++
@@ -1,9 +0,0 @@
-const { drizzle } = require('drizzle-orm/postgres-js');
-const postgres = require('postgres');
-
-// Simple PostgreSQL connection
-const connectionString = process.env.DATABASE_URL;
-const client = postgres(connectionString);
-const db = drizzle(client);
-
-module.exports = { db };
